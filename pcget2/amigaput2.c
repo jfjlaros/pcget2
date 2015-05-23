2594c0044792d65209000e889ca860ac0ac493ef
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2015-05-23 14:44:24 +0200

    Initial commit.

---
+++ pcget2/amigaput2.c
@@ -0,0 +1,463 @@
+/* 
+    amigaput: by Greg Craske 18/6/2001 (Freeware)
+    Uses parallel port to send files to an Amiga that is running 'pcget'.
+    Used with PS/2 compatible parallel ports.  Non-PS/2 ports will not
+    allow the Amiga to send back error messages.
+
+    Cable assembly instructions at 
+      http://www.cs.rmit.edu.au/~craske/amiga
+
+    Written specifically for Linux, compile with 
+      gcc -O2 -o amigaput amigaput.c
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <asm/io.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* BASEPORT addresses: specific to LPT1 */
+#define DATAP 0x378
+#define STATP 0x379
+#define CONTP 0x37a
+
+
+/* STATEMENTS TO DO PLATFORM SPECIFIC OPERATIONS 
+   RTS from PC or Amiga and ACK from Amiga is 'edge triggered (either edges)', 
+   STR from PC is 'pulse triggered (off-on-off)' */
+
+#define REMOTE_RTS (inb(STATP)&0x10)
+#define TOGGLE_RTS outb(inb(CONTP)^0x02,CONTP)
+#define STR outb(inb(CONTP)|0x01,CONTP);outb(inb(CONTP)&0xfe,CONTP)
+#define ACK (inb(STATP)&0x20)
+#define READBYTE (inb(DATAP))
+#define WRITEBYTE outb(writebyte,DATAP)
+#define SETREADMODE outb(inb(CONTP)|0x20, CONTP)
+#define SETWRITEMODE outb(inb(CONTP)&0xdf, CONTP)
+
+/* LINUX SPECIFIC PERMISSION OPERATIONS TO USE IO PORT*/
+#define GETPORTPERMS if (ioperm(DATAP, 3, 1)) { perror("ioperm: couldn't get permissions for registers."); exit(1);}
+#define RELPORTPERMS if (ioperm(DATAP, 3, 0) ) { perror("ioperm: couldn't release permissions for registers."); exit(1); }
+
+
+
+/* SPECIAL DATA BYTES */
+#define FILENAME_HDR 0xaa
+#define LENGTH_HDR 0xab
+#define ERROR 0xee
+#define END_ALL 0xff
+
+
+/* ERROR CODES */
+#define NO_ERR 0
+#define ERR_RTS_CONFLICT 1 /* Both computers tried to send simultaneously */
+#define ERR_ACK_TIMEOUT 2  /* Timed out waiting for ack from receiver */
+#define ERR_RECEIVE_SIDE 3 /* Receiver sent back an error condition */
+#define ERR_FILE_ERROR 11
+#define ERR_REC_FILE_EXISTS 4
+#define ERR_REC_BAD_CHECKSUM 5
+
+void printerr(int err)
+{
+  switch (err) 
+  {
+    case ERR_RTS_CONFLICT: 
+     printf("\nERROR %d: RTS Conflict between computers!\n", ERR_RTS_CONFLICT); 
+     break;
+    case ERR_ACK_TIMEOUT: 
+     printf("\nERROR %d: ACK timed out! Check receiver.\n", ERR_ACK_TIMEOUT);
+     break;
+    case ERR_RECEIVE_SIDE: 
+     printf("\nERROR %d: General receiver-side error!\n", ERR_RECEIVE_SIDE);
+     break;
+    case ERR_REC_FILE_EXISTS: 
+     printf("\nERROR %d: File exists!\n", ERR_REC_FILE_EXISTS);
+     break;
+    case ERR_REC_BAD_CHECKSUM: 
+     printf("\nERROR %d: Bad checksum from receiver!\n", ERR_REC_BAD_CHECKSUM);
+     break;
+    case ERR_FILE_ERROR: 
+     printf("\nERROR %d: Receiver can't create file!\n", ERR_FILE_ERROR);
+     break;
+    case NO_ERR: break;
+     default: printf("\nUnknown error: %x\n", err);
+     break;
+  }
+}
+    
+
+/* Takes byte to be sent, sends and does the handshaking.
+   Returns error code, or 0 if no errors */
+int senddata(int dout);
+
+/* Takes filename to be sent.  
+   Returns error code, or 0 if no errors */
+int send_filename(char *fname);
+
+/* Takes file to get size of.  
+   Returns error code, or 0 if no errors */
+int send_filesize(char *fname);
+
+/* Takes calculated checksum.
+   Returns error code, or 0 if all good.. */
+int send_checksum(int checksum); 
+
+/* Simply waits for ack signal.
+   Returns errorcode if time out or remote RTS .  0 otherwise */
+int wait_for_ack();
+
+/* On getting remote RTS, attempt to receive error message.
+   Returns error code, and prints to stdout the message. */
+int get_error();
+
+/* Reads filesizes
+   (taken from 'The C Programming Language', by B.W.Kernighan & D.M.Ritchie)*/
+int fsize(char *);
+
+
+/* Naughty naughty global vars */
+unsigned char oldack;
+unsigned char oldremrts;
+
+int main (int argc, char *argv[])
+{
+   int filenum = 0;
+   int errors = 0;
+   int endall = 0;
+   unsigned short checksum = 0;
+   int sumbyte;
+   int newsum;
+   char writebyte;
+
+   long i = 0;
+   char contr;
+   FILE *infile;
+   int inc = ' ';
+   int byt = 0;
+   int kbt = 0;
+   int filesize;
+
+
+   /* CHECK FOR PARAMS */
+   if (argc < 2) 
+   { 
+     printf("Usage: %s {file} [{file(s)}]\n", argv[0]); 
+     exit(1);
+   }
+   
+   /* GET PORT PERMISSIONS (linux) */
+   GETPORTPERMS
+
+   oldremrts = REMOTE_RTS; /* Set old RTS value */
+   printf("Amiga Sender Program (via parallel port) by Greg Craske.\n");
+   printf("Waiting for receiver.");
+   fflush(stdout);
+
+   while (REMOTE_RTS == oldremrts && i++ < 20)
+   {
+     sleep(1);
+     putchar('.');
+     fflush(stdout);
+   }
+   if (i >= 20)
+   {
+     errors = ERR_ACK_TIMEOUT;
+     printf("\nNo response! Exiting.\n");
+     endall = 1;
+   }
+   else
+     printf("\nOK.\n\n");
+
+   oldremrts = REMOTE_RTS; /* Set old RTS value */
+
+   SETWRITEMODE;
+
+   /* MAIN LOOP FOR EACH FILE */
+   while (++filenum < argc && !endall)
+   {
+     checksum = 0;
+     sumbyte = 0;
+     newsum = 0;
+     oldack = ACK;
+
+     if ((infile = fopen(argv[filenum], "r")) == NULL)
+     {
+       fprintf(stderr, "\nCan't open file %s.\n", argv[filenum]);
+     }
+     else
+     { 
+       errors = 0;
+       byt = 0;
+       kbt = 0;
+       printf("\nSending %s, waiting for ack...\n", argv[filenum]);
+
+
+       writebyte = 0xaa; WRITEBYTE;          /* Write start data */
+
+       TOGGLE_RTS;
+       ACK; /* Set old ACK value */
+       errors = wait_for_ack();
+
+       /* SEND FILENAME */
+       if (!errors) errors = senddata(0xaa);
+       if (!errors) errors = send_filename(argv[filenum]);
+
+       /* Send File Size */
+       if (!errors) errors = senddata(0xab);
+       if (!errors) errors = send_filesize(argv[filenum]);
+
+       if (!errors) errors = senddata(0);  /* Read to send data block */
+
+       if (!errors)
+         printf("OK. Kbytes sent:\n0");
+
+       /* DATA SENDING LOOP */
+       byt = 0;
+       checksum = 0;
+       while(!errors && ((inc = getc(infile)) != EOF)) 
+       {
+         errors = senddata(inc);
+
+         /* calc checksum */
+         if (byt & 0x01)
+           checksum = checksum + inc;
+         else
+           checksum = checksum + (inc << 8);
+         
+         /* print amount sent (in 2k intervals) */
+         if (++byt >= 2048)
+         {
+           kbt+=2;
+           printf("\b\b\b\b\b%d", kbt);
+           fflush(stdout);
+           byt = 0;
+         }
+       }
+
+       if (errors)
+       {
+         if (errors == ERR_RTS_CONFLICT)
+           get_error();
+
+         if (errors == ERR_REC_BAD_CHECKSUM ||
+             errors == ERR_FILE_ERROR)
+           endall = 0;
+         else
+           endall = 1;
+
+         printerr(errors);
+         sleep(1);
+       }
+       else
+       {
+         send_checksum(checksum);
+       }
+
+
+       if (!errors) 
+       {
+         if (byt >= 1024)
+           kbt++;
+         printf("\b\b\b\b\b%d\n", kbt);
+       }
+
+     } /* end: IF the file could be concat(else)*/
+
+     TOGGLE_RTS;
+     errors = wait_for_ack();
+     fclose (infile);
+
+   } /* end: WHILE there are more files to send */
+
+   /* SEND FINISH */ 
+   if (errors == NO_ERR || errors == ERR_REC_BAD_CHECKSUM ||
+                           errors == ERR_FILE_ERROR)
+   {
+     printf("Sending end...\n"); 
+     TOGGLE_RTS;
+     senddata(END_ALL);
+     TOGGLE_RTS;
+     printf("\nende!\n");
+   }   
+
+   /* RELEASE PORT PERMISSIONS */
+   RELPORTPERMS
+
+   exit (0);
+} /* END MAIN */
+
+/* =================================================== */
+
+int senddata(int dout)
+{
+  int nogo = 0;
+  long cnt = 0;
+  char writebyte;
+
+  writebyte = (char)dout;
+  WRITEBYTE;
+ 
+  STR;  /* send strobe */
+
+  while(ACK == oldack)
+  {
+    if (REMOTE_RTS != oldremrts)
+      return get_error();
+
+    /* dodgy processor wait loop to wait for ACK from Amiga 
+       (should've created a separate timing process but I'm too lazy) */
+    if (++cnt == 100000)
+    {
+      STR;
+      cnt = 0;
+
+      if (nogo++ >= 80) 
+      {
+        return ERR_ACK_TIMEOUT;
+        printf("  waiting for receiver...\n");
+      }
+    }
+  }
+
+  oldack = ACK;
+  return NO_ERR;
+}
+
+/* ------------------------------------------------*/
+
+int send_filename(char *fname)
+{
+  int errors = 0;
+
+  while(*fname != '\0' && *fname != '\n' && !errors)
+    errors = senddata(*fname++);
+
+  return NO_ERR;
+}
+
+/* ------------------------------------------------*/
+
+int send_filesize(char *fname)
+{
+  int filesize = 0;
+  int piece;
+  int i = 0;
+  int errors = NO_ERR;
+
+  if ((filesize = fsize(fname)) == 0)
+  {
+    return NO_ERR;
+  }
+
+  piece = filesize & 0xff;
+  while (piece && !errors)
+  {
+    piece = piece >> (8*i);
+    errors = senddata(piece);
+    i++;
+    piece = filesize & (0xff << (8*i));
+  }
+  
+  return errors;
+}
+
+/* ------------------------------------------------*/
+
+int send_checksum(int checksum)
+{
+  int errors = senddata(checksum&0xff);
+
+
+  if (!errors)
+    errors = senddata( (checksum & 0xff00) >> 8);
+
+  return errors;
+}
+
+/* ------------------------------------------------*/
+
+int wait_for_ack()
+{
+  int nogo = 0;
+  long sleepcount = 0;
+  int newack;
+
+
+  while((newack = ACK) == oldack)
+  {
+    /* check if Amiga is trying to send to PC */
+    if (REMOTE_RTS != oldremrts)
+    { 
+      /* If so, it's probably sending an error message */
+      return get_error();
+    }
+
+    sleep(1);
+
+    STR; /* In case Amiga didn't get previous one */
+
+    if (sleepcount++ == 2)
+    {
+      sleepcount = 0;
+      if (nogo++ >= 10) /* if effectively waiting for 20 seconds.. */
+        return ERR_ACK_TIMEOUT;
+
+      printf("still waiting for ack...\n");
+    }
+  }
+  oldack = newack;
+  return NO_ERR;
+}
+
+/* ------------------------------------------------*/
+
+int get_error()
+{
+  int din;
+  int writebyte = 0xaa;  
+
+  SETREADMODE;
+
+  oldremrts = REMOTE_RTS;
+
+  TOGGLE_RTS; /* make RTS go down */
+  
+  wait_for_ack();
+  if ((din = READBYTE) != ERROR)
+    return ERR_RTS_CONFLICT;
+  STR;  
+
+  wait_for_ack();
+  din = READBYTE;
+  STR;
+
+  while (REMOTE_RTS == oldremrts);
+  TOGGLE_RTS; /* make rts go up */
+
+  oldremrts = REMOTE_RTS;
+
+  SETWRITEMODE;
+  return din;
+}
+
+/* ------------------------------------------------*/
+
+int fsize (char *name)
+{
+  struct stat stbuf;
+
+  if (stat(name, &stbuf) == -1)
+  {
+    fprintf(stderr, "fsize: can't access %s\n", name);
+    return 0;
+  }
+
+  if ((stbuf.st_mode & S_IFMT) == S_IFDIR)
+    return 0;
+  else
+    return stbuf.st_size;
+}
+
---
+++ pcget2/pcget2.c
@@ -0,0 +1,460 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* DISK WRITE*/
+#include <exec/ports.h>
+#include <exec/io.h>
+#include <exec/memory.h>
+#include <devices/trackdisk.h>
+
+#define NUMTRKS 20
+#define TD_BUFSIZE (11 * 1024 * NUMTRKS)
+#define NUMBLOCKS (NUMTRKS * 22)
+
+typedef unsigned long ulong;
+typedef unsigned char uchar;
+
+struct MsgPort *td_mp;
+struct IOStdReq *td_iob;
+
+extern struct MsgPort *CreatePort();
+extern struct IOStdReq *CreateStdIO();
+extern long OpenDevice();
+extern void *AllocMem();
+/* END DISK WRITE*/
+
+#define DATAP 0xBFE101
+#define CONTP 0xBFD000
+
+#define byte unsigned char
+#define CIABpra *(byte*)0xbfd000
+#define CIABddra *(byte*)0xbfd200
+#define CIABprb *(byte*)0xbfd100
+#define CIABddrb *(byte*)0xbfd300
+#define CIABtodlo *(byte*)0xbfd800
+#define CIABicr *(byte*)0xbfdd00
+
+#define CIAApra *(byte*)0xbfe001
+#define CIAAddra *(byte*)0xbfe201
+#define CIAAprb *(byte*)0xbfe101
+#define CIAAddrb *(byte*)0xbfe301
+#define CIAAtodlo *(byte*)0xbfe801
+#define CIAAicr *(byte*)0xbfed01
+
+#define word unsigned short
+#define INTENA *(word*)0xdff09a
+#define INTENAR *(word*)0xdff01c
+#define DMACON *(word*)0xdff096
+#define DMACONR *(word*)0xdff002
+#define INTREQR *(word*)0xdff01e
+#define INTREQ *(word*)0xdff09c
+
+#define REMOTE_RTS
+#define TOGGLE_RTS (CIABpra=CIABpra^(byte)0x04)
+#define STR (CIABpra=CIABpra^(byte)0x02)
+#define ACK (CIAAicr & (byte)0x10)
+#define READBYTE CIAAprb
+#define WRITEBYTE CIAAprb=writebyte
+#define SETREADMODE CIAAddrb=(byte)0x00
+#define SETWRITEMODE CIAAddrb=(byte)0xff
+
+/* ERROR CODES */
+#define NO_ERR 0
+#define ERR_BAD_FORMAT 10
+#define ERR_FILE_ERROR 11
+
+void send_error(byte ernum, byte oldlook);
+
+/* #define BUFSIZE 32768                                 DISK WRITE */
+#define WAITLOOP 100000
+
+main(int argc, char *argv[]) {
+  byte dat;
+  byte lookstart;
+  word moredata;
+  word morefiles;
+  int filesizer;
+  int i = 0;
+  word j = 0;
+  int bi = 0;
+  int by = 0;
+  int outfile;
+  int writ = 0;
+  int incount = 0;
+  byte *outbuf;
+  int nread = 0; 
+  short prenread = 0;
+  word errors = NO_ERR;
+  byte checkin0;
+  byte checkin1;
+
+  unsigned short checksum = 0;
+  unsigned short checksumin = 0;
+  char filename[256];
+
+  /* DISK WRITE*/
+  int BUFSIZE = TD_BUFSIZE;
+  int diskwrite = 0;
+  long track = 0;
+  int err = 0;
+  /* END DISK WRITE*/
+
+  printf("PC Receiver Program (via parallel port) by Greg Craske.\n"); 
+
+
+  /* DISK WRITE*/
+  if ((argv[1][0] == '-') && (argv[1][1] == 'w')) {
+    BUFSIZE = TD_BUFSIZE; 
+    diskwrite = 1;
+
+    fprintf(stderr,"Insert disk and hit RETURN to continue:\n");
+    getchar();
+
+    td_mp = CreatePort(0L,0L);
+    if(!td_mp) exit(99);
+    td_iob = CreateStdIO(td_mp);
+    if(!td_iob) {
+      DeletePort(td_mp);
+      exit(99);
+    }
+    if(OpenDevice("trackdisk.device",0L,td_iob,0L)) {
+      printf("Unable to open floppy device driver.\n");
+      DeleteStdIO(td_iob);
+      DeletePort(td_mp);
+      exit(99);
+    }
+
+    outbuf = AllocMem(TD_BUFSIZE,MEMF_PUBLIC|MEMF_CHIP);
+    if(!outbuf) {
+      printf("Unable to obtain %ld byte buffer in chip memory.\n",TD_BUFSIZE);
+      CloseDevice(td_iob);
+      DeleteStdIO(td_iob);
+      DeletePort(td_mp);
+      exit(99);
+    }
+    td_iob->io_Command = TD_MOTOR;
+    td_iob->io_Length = 1;
+    DoIO(td_iob);
+    td_iob->io_Command = TD_FORMAT;
+    td_iob->io_Data = (APTR) outbuf;
+    td_iob->io_Length = TD_BUFSIZE;
+  }
+  else
+    if ((outbuf = (char *)malloc(BUFSIZE)) == NULL)
+    {
+      perror("Can't allocate buffer!\n");
+      exit(1);
+    }
+  /* END DISK WRITE*/
+
+  CIAAddrb = (byte)0x00;  /*set DDR for data pins to INPUT */
+  CIABddra = (byte)0xfe;  /*set DDR for control/status pins */
+  lookstart = CIABpra;   /*get initial crl/stat */
+
+  morefiles = 1;
+
+  i = 0;
+  j = 0;
+  printf("Waiting for sender.");
+  fflush(stdout);
+
+  while(i < WAITLOOP)
+  {
+    for (i = 0; i < WAITLOOP-1; i++) 
+      if ( (CIABpra & 0x01) != (lookstart & 0x01))
+      {
+        i = WAITLOOP;
+        printf("\nOK\n\n");
+      }
+
+    if (i < WAITLOOP)
+    {
+      printf(".");
+      fflush(stdout);
+      TOGGLE_RTS;
+    }
+    if (j++ >= 20)
+    {
+      printf("\nWaiting timed out.  Exiting.\n");
+      morefiles = 0;
+      i = WAITLOOP;
+    }
+  }
+
+
+  while (morefiles)
+  { 
+
+    /* wait for begin txn toggle */
+    while(i < WAITLOOP)
+    {
+      for (i = 0; i < WAITLOOP-1; i++)
+      {
+        if ( (CIABpra & 0x01) != (lookstart & 0x01))
+          i = WAITLOOP;
+      }
+
+      if (i < WAITLOOP)
+        printf("waiting for begin send..\n");
+    }
+
+    if (morefiles == 1)
+      printf("found send request.  receiving....\n");
+
+    /* MAIN RECEIVE LOOP */
+    moredata = 1;
+    i = 0;
+    writ = 0;
+    /*lookstart = CIABpra;*/
+    errors = 0;
+    checksum = 0;
+    nread = 0;
+    prenread = 0;
+    checksumin = 0;
+    filesizer = 0;
+
+    while(moredata)
+    {
+      /* WAIT FOR DATA CLKIN OR RTS GO LOW */
+      while ( !(CIAAicr & 0x10) && ((lookstart & 0x01) != (CIABpra & 0x01)) )
+        if(!(CIAApra & 0x80)) /* EMERGENCY QUIT (MOUSE BUTTON) */
+        {
+          moredata = 0;
+          morefiles = 0;
+          break;
+        }
+
+      if ((lookstart & 0x01) == (CIABpra & 0x01)) /* TEST IF IT WAS THE RTS */
+      {
+        CIABpra = CIABpra^(byte)0x02; /*send ack */
+        moredata = 0;  /* end transmission */
+        if (writ != 4)
+          errors = ERR_BAD_FORMAT;
+      }
+      else
+      {
+        /* FOUND DATA CLKIN, GET DATA AND SEND ACK */
+        dat = CIAAprb;       /*get new data */
+        CIABpra = CIABpra^(byte)0x02; /*send ack */
+
+        switch (writ) /* WOT TO DO WITH THE DATA */
+        {
+          case 0: if (dat == 0xaa)
+                    break; 
+                  else
+                    if (dat == 0xff)
+                    {
+                      morefiles = 0;
+                      moredata = 0;
+                      break;
+                    }
+                    else
+                      writ = 1;
+
+
+          case 1: if (dat == 0xab)
+                    { 
+                      /* DISK WRITE*/
+                      if (!diskwrite) {
+                        filename[i] = '\0';
+
+                        if (morefiles < argc)
+                        {
+                          printf("Writing file %s ", argv[morefiles]);
+                          if ((outfile = creat(argv[morefiles], 0666)) == -1)
+                          {
+                            perror("- Can't open file!\n");
+                            send_error(ERR_FILE_ERROR, lookstart);
+                            moredata = 0;
+                          }
+                        }
+                        else
+                        { 
+                          printf("Writing file %s ", filename);
+                          if ((outfile = creat(filename, 0666)) == -1)
+                          {
+                            perror("- Can't write file!\n");
+                            send_error(ERR_FILE_ERROR, lookstart);
+                            moredata = 0;
+                          }
+                        }
+                      }
+
+                      morefiles++;
+                      writ = 2;  
+                      i = 0;   
+                    }
+                  else 
+                    if (i < 255)
+                    {
+                      filename[i++] = dat;
+                    } 
+                  break;
+          case 2: if (dat == 0)
+                    { 
+                      printf("(%d bytes)\n", filesizer);
+                      writ = 3;
+                    }
+                    else if (i < 8)
+                    {
+                      filesizer+= dat << (8 * i++);
+                    }
+                  break;
+          case 3: 
+                  if (prenread++ == 0)
+                    checkin0 = dat;
+                  else
+                  {
+                    checkin1 = dat;
+                    writ = 4;
+                  }
+                  break;
+          case 4:
+                  if (nread&0x01)
+                  {
+                    outbuf[nread] = checkin1;
+                    checksum += checkin1;
+                    checkin1 = dat;
+
+                  }
+                  else
+                  {
+                    outbuf[nread] = checkin0;
+                    checksum += checkin0 << 8;
+                    checkin0 = dat;
+                  }
+
+                  if (++nread == BUFSIZE)
+                  {
+                    /* DISK WRITE*/
+                    if (diskwrite) {
+                      printf("Writing tracks %ld to %ld.\r", track,
+                          track + NUMTRKS - 1);
+                      fflush(stdout);
+                      td_iob->io_Offset = 11 * 1024 * track;
+                      DoIO(td_iob);
+                      if(td_iob->io_Error) 
+                        printf("Floppy write failed, error number %d.\n",
+                            td_iob->io_Error);
+                      track += NUMTRKS;
+                    }
+                    else
+                      /* END DISK WRITE*/
+                      write(outfile, outbuf, nread);
+                    nread = 0;
+                    break;            
+                  }
+          default: break;
+        } /* END SWICH */
+      } /* END IF */   
+    } /* END LOOP */
+
+    if (morefiles)
+    {
+      if (nread&0x01)
+        checksumin = (int)((checkin0 &0xff)<<8) + (int)(checkin1 &0xff);
+      else
+        checksumin = (int)((checkin1 &0xff)<<8) + (int)(checkin0 &0xff);
+
+      if (!errors)
+      {
+        if (checksum != checksumin)
+          printf("CHECKSUM ERROR (received %x, calculated %x)\n", checksumin, checksum);
+
+        if (nread != 0)
+        {
+          /* DISK WRITE*/
+          if (diskwrite) {
+            printf("Writing tracks %ld to %ld.\r", track, track + NUMTRKS - 1);
+            fflush(stdout);
+            td_iob->io_Offset = 11 * 1024 * track;
+            DoIO(td_iob);
+            if(td_iob->io_Error) 
+              printf("Floppy write failed, error number %d.\n",
+                  td_iob->io_Error);
+          }
+          else
+            /* END DISK WRITE*/
+            write(outfile, outbuf, nread);
+        }
+        printf("  done.\n");
+      }
+      close(outfile);
+    }
+    else
+      printf("ende!\n");
+  }
+  /* DISK WRITE*/
+  if (diskwrite) {
+    printf("                             \r");
+    fflush(stdout);
+
+    FreeMem(outbuf,TD_BUFSIZE);
+
+    td_iob->io_Command = CMD_UPDATE;
+    DoIO(td_iob);
+    if(td_iob->io_Error) printf("Floppy update failed, error number %d.\n",
+        td_iob->io_Error);
+
+    td_iob->io_Command = TD_MOTOR;
+    td_iob->io_Length = 0;
+    DoIO(td_iob);
+
+    CloseDevice(td_iob);
+    DeleteStdIO(td_iob);
+    DeletePort(td_mp);
+  }
+  else
+    /* END DISK WRITE*/
+    free(outbuf);
+
+
+  CIAAprb = (byte)0xff;
+  CIAAddrb = (byte)0x00;
+  CIABpra = (byte)0xfe;
+  CIABddra = (byte)0xc0;
+}
+
+void smalldelay()
+{
+  byte timerb;
+
+  timerb = CIABtodlo & 0x80;
+  while ((CIABtodlo & 0x80) == timerb);
+  timerb = CIABtodlo & 0x80;
+  while ((CIABtodlo & 0x80) == timerb);
+}
+
+void send_error(byte ernum, byte oldlook)
+{
+  byte writebyte;  
+  int i = 30000;
+
+  TOGGLE_RTS; /* send rts */
+
+  writebyte = 0xee;
+  WRITEBYTE;
+
+  /* wait until remote rts goes */
+  while ((oldlook & 0x01) != (CIABpra & 0x01)); 
+
+  SETWRITEMODE;
+
+  STR;
+  while (!ACK);
+
+  writebyte = ernum;
+  WRITEBYTE;
+
+  smalldelay();
+
+  STR;
+  while (!ACK);
+
+  SETREADMODE;
+
+  TOGGLE_RTS;
+
+  /* wait until remote rts comes */
+  while ((oldlook & 0x01) == (CIABpra & 0x01));
+}
